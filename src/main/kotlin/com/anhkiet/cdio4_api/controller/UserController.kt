d4a5c5dec8655ee0a2c5c5e9ba7b04978461bc47
“NgocNV” <ngocdev.1812@gmail.com>
2023-09-25 17:37:36 +0700

    add user api

---
+++ src/main/kotlin/com/anhkiet/cdio4_api/controller/UserController.kt
@@ -0,0 +1,28 @@
+package com.anhkiet.cdio4_api.controller
+
+import com.anhkiet.cdio4_api.helper.responseHelper.content
+import com.anhkiet.cdio4_api.helper.responseHelper.response
+import com.anhkiet.cdio4_api.model.HouseResponseModel
+import com.anhkiet.cdio4_api.model.UserResponseModel
+import com.anhkiet.cdio4_api.service.AccountService
+import com.anhkiet.cdio4_api.service.HouseService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/user")
+class UserController(
+        private val accountService: AccountService
+) {
+    @GetMapping("/getByEmail")
+    fun getUserByEmail(email: String) = response {
+        val user = accountService.findByEmail(email)
+        content(
+                UserResponseModel(
+                        user != null,
+                        user
+                )
+        )
+    }
+}
